84db832377cf3353ca18eaaf9adee3dd262b1281
Mohammad Javad Ahmadi <36800130+MJAHMADEE@users.noreply.github.com>
2024-06-14 13:49:58 +0330

    Add files via upload

---
+++ Chapter 11 - 12/01 PID Control/CL_Dynamic_Solver.m
@@ -0,0 +1,24 @@
+%% 
+% 	Fundamentals of Robotics: Practical Case Studies with Matlab & Python
+%   By Prof. Hamid D. Taghirad
+%   Copyright CRC - Press @2025
+%
+%   This code Runs the closed loop simulation of 3R manipulator
+%   with PID controller
+%   
+%% Initializing
+clear ;
+clc ; clf ; cla;
+
+%% Determining the Structure 
+
+[SP] = Structural_Parameters() ;
+
+%% Solve The Dynamic Equation
+
+[t,q] = ode23s(@(t,qs) CL_Dynamics(t,qs,SP), SP.tspan, ...
+    [SP.q0 ; SP.qd0; SP.qi0; SP.xi0] ... % initial condition for intergrations
+    ,odeset('OutputFcn','odeplot','OutputSel',[1:3])) ;
+
+%% Ploting and Exporting the Results
+[Out]=PlotData(t,q,SP);
---
+++ Chapter 11 - 12/04 PIDC Control/CL_Dynamic_Solver.m
@@ -0,0 +1,25 @@
+%% 
+% 	Fundamentals of Robotics: Practical Case Studies with Matlab & Python
+%   By Prof. Hamid D. Taghirad
+%   Copyright CRC - Press @2025
+%
+%   This code Runs the closed loop simulation of 3R manipulator
+%   with Partial IDC + PID 
+%   
+%% Initializing
+clear ;
+clc ; clf ; cla;
+tic
+
+%% Determining the Structure 
+
+[SP] = Structural_Parameters() ;
+
+%% Solve The Dynamic Equation
+
+[t,q] = ode23s(@(t,qs) CL_Dynamics(t,qs,SP), SP.tspan, ...
+    [SP.q0 ; SP.qd0; SP.qi0; SP.xi0] ... % initial condition for intergrations
+    ,odeset('OutputFcn','odeplot','OutputSel',[1:3])) ;
+toc
+%% Ploting and Exporting the Results
+[Out]=PlotData(t,q,SP);
